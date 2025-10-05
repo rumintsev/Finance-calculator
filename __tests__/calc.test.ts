de0c5d27432b016e9e4fc415756088e77b3a1f6e
rumintsev <mihailru2004@gmail.com>
2025-10-05 11:40:43 +0300

    Coverage script update

--- __tests__/calc.test.ts
+++ __tests__/calc.test.ts
@@ -1,14 +1,14 @@
-import { calcDepositProfit, calcFundProfit } from "../src/lib/calc";
+import { calcDepositProfit, calcFundProfit } from '../src/lib/calc';
 
-describe("calcDepositProfit", () => {
-  test("Deposit within the tax-free limit 1 month", () => {
+describe('calcDepositProfit', () => {
+  test('Deposit within the tax-free limit 1 month', () => {
     const options = {
       amount: 100_000,
       rate: 12,
       months: 1,
       taxRate: 10,
       taxFreeAmount: 1000,
-      startDate: new Date("2025-01-01"),
+      startDate: new Date('2025-01-01'),
       earnedThisYear: 0,
     };
 
@@ -17,14 +17,14 @@ describe("calcDepositProfit", () => {
     expect(Math.round(result)).toBe(101_000);
   });
 
-  test("Deposit within the tax-free limit 10 month", () => {
+  test('Deposit within the tax-free limit 10 month', () => {
     const options = {
       amount: 100_000,
       rate: 12,
       months: 10,
       taxRate: 10,
       taxFreeAmount: 11_000,
-      startDate: new Date("2025-01-01"),
+      startDate: new Date('2025-01-01'),
       earnedThisYear: 0,
     };
 
@@ -33,14 +33,14 @@ describe("calcDepositProfit", () => {
     expect(Math.round(result)).toBe(110_462);
   });
 
-  test("Deposit outside the tax-free limit", () => {
+  test('Deposit outside the tax-free limit', () => {
     const options = {
       amount: 100_000,
       rate: 12,
       months: 2,
       taxRate: 10,
       taxFreeAmount: 1000,
-      startDate: new Date("2025-01-01"),
+      startDate: new Date('2025-01-01'),
       earnedThisYear: 0,
     };
 
@@ -49,14 +49,14 @@ describe("calcDepositProfit", () => {
     expect(Math.round(result)).toBe(101_909);
   });
 
-  test("Resetting the tax-free limit in the new year", () => {
+  test('Resetting the tax-free limit in the new year', () => {
     const options = {
       amount: 100_000,
       rate: 12,
       months: 3,
       taxRate: 10,
       taxFreeAmount: 1000,
-      startDate: new Date("2025-10-01"),
+      startDate: new Date('2025-10-01'),
       earnedThisYear: 0,
     };
 
@@ -65,14 +65,14 @@ describe("calcDepositProfit", () => {
     expect(Math.round(result)).toBe(102_926);
   });
 
-  test("Something has already been earned on the deposit", () => {
+  test('Something has already been earned on the deposit', () => {
     const options = {
       amount: 100_000,
       rate: 12,
       months: 1,
       taxRate: 13,
       taxFreeAmount: 1000,
-      startDate: new Date("2025-01-01"),
+      startDate: new Date('2025-01-01'),
       earnedThisYear: 600,
     };
 
@@ -81,14 +81,14 @@ describe("calcDepositProfit", () => {
     expect(Math.round(result)).toBe(100_922);
   });
 
-  test("Boundary case: zero rate", () => {
+  test('Boundary case: zero rate', () => {
     const options = {
       amount: 100_000,
       rate: 0,
       months: 12,
       taxRate: 13,
       taxFreeAmount: 500,
-      startDate: new Date("2025-01-01"),
+      startDate: new Date('2025-01-01'),
       earnedThisYear: 0,
     };
 
@@ -97,14 +97,14 @@ describe("calcDepositProfit", () => {
     expect(result).toBe(100_000);
   });
 
-  test("Boundary case: zero months", () => {
+  test('Boundary case: zero months', () => {
     const options = {
       amount: 100_000,
       rate: 10,
       months: 0,
       taxRate: 13,
       taxFreeAmount: 500,
-      startDate: new Date("2025-01-01"),
+      startDate: new Date('2025-01-01'),
       earnedThisYear: 0,
     };
 
@@ -114,8 +114,8 @@ describe("calcDepositProfit", () => {
   });
 });
 
-describe("calcFundProfit", () => {
-  test("Correct calculation of profit from the fund", () => {
+describe('calcFundProfit', () => {
+  test('Correct calculation of profit from the fund', () => {
     const options = {
       amount: 100_000,
       rate: 17,
@@ -128,7 +128,7 @@ describe("calcFundProfit", () => {
     expect(Math.round(result)).toBe(103_656);
   });
 
-  test("Boundary case: 100% tax rate", () => {
+  test('Boundary case: 100% tax rate', () => {
     const options = {
       amount: 100_000,
       rate: 12,
